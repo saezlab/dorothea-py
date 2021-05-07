68e64c1d654b9f451c73c70797aeab43e6795cf7
Pau Badia i Mompel <paubadiamompel@gmail.com>
2021-05-07 18:38:43 +0200

    Fixed pip bug

--- setup.py
+++ setup.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python
-import setuptools
+
+from distutils.core import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
-setuptools.setup(
-    name="dorothea-py",
-    version="1.0.1",
-    author="Pau Badia i Mompel",
+
+setup(
+    name='dorothea-py',
+    version='1.0.2',
+    author='Pau Badia i Mompel',
     author_email="pau.badia@uni-heidelberg.de",
-    description="dorothea-py is a python package to compute TF activity \
-    from RNA-seq data using DoRothEA as regulon resource",
+    description='dorothea-py is a python package to compute TF activity \
+    from RNA-seq data using DoRothEA as regulon resource',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/saezlab/dorothea-py",
+    url='https://github.com/saezlab/dorothea-py',
     project_urls={
         "Bug Tracker": "https://github.com/saezlab/dorothea-py/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "dorothea"},
-    packages=setuptools.find_packages(where="dorothea"),
-    python_requires=">=3.6",
-    package_data={
-        'dorothea': ['data/dorothea_hs.pkl', 
-                     'data/dorothea_mm.pkl', 
-                     'data/c_dorothea_hs.pkl', 
-                     'data/c_dorothea_mm.pkl']
-    },
+    packages=['dorothea'], 
+    license='LICENSE.txt',
+    package_data={'dorothea': ['data/dorothea_hs.pkl', 
+                              'data/dorothea_mm.pkl', 
+                              'data/c_dorothea_hs.pkl', 
+                              'data/c_dorothea_mm.pkl']
+                },
     install_requires=[
         'anndata',
         'scanpy',
